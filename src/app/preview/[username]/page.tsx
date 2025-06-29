d118d8e8729055898a4429753b1cbbc69e064b30
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-06-29 05:23:13 +0300

    Refactor user public profile data fetching

--- src/app/preview/[username]/page.tsx
+++ src/app/preview/[username]/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import { ObjectId } from "mongodb";
 import { notFound, redirect } from "next/navigation";
 
-import { Card } from "@/components/ui/card";
+import connectToDatabase from "@/lib/db";
+import { LinkDocument, UserDocument, PublicUserProfile, Link as TLink } from "@/lib/types";
 
-import { getUserById, getUserLinks } from "../actions/preview";
+import { Card } from "@/components/ui/card";
 import { UserPublicLinks } from "./_components/public-user-links";
 import { UserPublicProfile } from "./_components/public-user-profile";
 
@@ -55,4 +57,54 @@ export default async function userPublicPreviewPage({ searchParams }: PageProps)
       </Card>
     </main>
   );
+}
+
+export async function getUserById(id: string) {
+  try {
+    const { db } = await connectToDatabase();
+    const collection = db.collection<UserDocument>("users");
+
+    // Validate the ID format
+    if (!ObjectId.isValid(id)) {
+      throw new Error("Invalid user ID");
+    }
+
+    const user = await collection.findOne({ _id: new ObjectId(id) });
+
+    if (!user) return null;
+
+    const transformedUser: PublicUserProfile = {
+      displayEmail: user.displayEmail,
+      username: user.username || "",
+      image: user.image || "",
+    };
+
+    return transformedUser;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return null;
+  }
+}
+
+export async function getUserLinks(id: string) {
+  try {
+    const { db } = await connectToDatabase();
+    const collection = db.collection<LinkDocument>("links");
+    const links = await collection
+      .find({ userId: id })
+      .sort({ order: 1 })
+      .project<TLink>({
+        _id: 0,
+        id: { $toString: "$_id" },
+        platform: 1,
+        url: 1,
+        order: 1,
+        createdAt: 1,
+      })
+      .toArray();
+    return links;
+  } catch (error) {
+    console.error("Error fetching user links:", error);
+    return [];
+  }
 }
